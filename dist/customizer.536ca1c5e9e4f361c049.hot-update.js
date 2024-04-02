981a5ec04f44d4b9a262c7798a678b70cf162118
Art Bekolli <abekolli57@gmail.com>
2024-04-02 23:32:02 +0200

    style changes

---
+++ dist/customizer.536ca1c5e9e4f361c049.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdate"]("customizer",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "0f23f87736bb663ab7f0"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=customizer.536ca1c5e9e4f361c049.hot-update.js.map
---
+++ dist/main.536ca1c5e9e4f361c049.hot-update.js
@@ -0,0 +1,32 @@
+"use strict";
+self["webpackHotUpdate"]("main",{
+
+/***/ "./styles/main.scss":
+/*!**************************!*\
+  !*** ./styles/main.scss ***!
+  \**************************/
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+    if(true) {
+      // 1712093162981
+      var cssReload = __webpack_require__(/*! ../../node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js */ "../../node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js")(module.id, {"locals":false});
+      module.hot.dispose(cssReload);
+      module.hot.accept(undefined, cssReload);
+    }
+  
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "0f23f87736bb663ab7f0"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=main.536ca1c5e9e4f361c049.hot-update.js.map
